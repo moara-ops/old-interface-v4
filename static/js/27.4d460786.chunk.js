503b3335877f929b231a6fb1eb91121efba02898
MoAra1 <Allianikulaporaheem@gmail.com>
2022-06-20 18:00:02 +0400

    Updates

---
+++ static/js/27.4d460786.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/widgets"]=this["webpackJsonp@uniswap/widgets"]||[]).push([[27],{1186:function(s,p){s.exports={messages:{}}}}]);
+//# sourceMappingURL=27.4d460786.chunk.js.map
